fb3fef9c7d53ac9045cf115767265caa8dec8a64
zaurky <zaurky@zeb.re>
2013-07-26 17:32:06 +0200

    dont show useless logs from fdupes

--- download_images.sh
+++ download_images.sh
@@ -4,5 +4,5 @@ BASEDIR=$(dirname $0)
 
 cd $BASEDIR
 
-source workspace/bin/activate; PYTHONPATH=$BASEDIR python bin/download_image.py && fdupes -r -1 images | awk '{print $2}' | xargs rm -f -; deactivate
+source workspace/bin/activate; PYTHONPATH=$BASEDIR python bin/download_image.py && fdupes -r -1 images 2> /dev/null | awk '{print $2}' | xargs rm -f -; deactivate
 
