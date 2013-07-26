fddd9411c54985da069dce6b2110040089444fe4
zaurky <zaurky@zeb.re>
2013-07-26 17:47:11 +0200

    initial version of setup.py and what it requires

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from os import path
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+README = path.abspath(path.join(path.dirname(__file__), 'README.md'))
+desc = 'A Python util to easily get pics from twitter'
+
+setup(
+    name='twitter-downldr',
+    version='0.0.1',
+    author='Zaurky',
+    author_email='zaurky@zeb.re',
+    description=desc,
+    long_description=open(README).read(),
+    license='GPLV2',
+    url='http://github.com/zaurky/twitter-downldr',
+    packages=['twitter-downldr'],
+    install_requires=[
+        'pexif>=0.13',
+        'requests>=1.2.3',
+        'requests-oauthlib>=0.3.2',
+        'twython>=3.0.0',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
