a3bef86b9b0a8e16ab0b0fa640fe1cc13f08cbf2
zaurky <zaurky@zeb.re>
2013-08-02 16:32:39 +0200

    pass all the excepts in a decorator + add 2 new methods get_lists and get_list_users

--- download_twitter/api.py
+++ download_twitter/api.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
 
 import sys
+from functools import wraps
 
 from twython import Twython
 from twython.exceptions import TwythonRateLimitError, TwythonError
 
 from requests_oauthlib import OAuth1Session
 from requests.exceptions import ConnectionError
+
+
+def exception_handler(method):
+    """ Handle exceptions that could happen here """
+
+    @wraps(method)
+    def _handle_exceptions(*args, **kwargs):
+        try:
+            return method(*args, **kwargs)
+        except ConnectionError, err:
+            print "    connection error, %s (%s, %s)" % (method, args, kargs)
+        except TwythonRateLimitError, err:
+            print err
+            sys.exit(-1)
+        except TwythonError, err:
+            print "    error, %s (%s, %s)" % (method, args, kargs)
+
+    return _handle_exceptions
 
 
 class API(object):
@@ -16,9 +35,7 @@ class API(object):
     """
 
     def __init__(self, config):
-        """
-        Init the twitter api and a requests with the good crehencials
-        """
+        """ Init the twitter api and a requests with the good crehencials """
         consumer_key = config.get('main', 'consumer_key')
         consumer_secret = config.get('main', 'consumer_secret')
         access_key = config.get('main', 'access_key')
@@ -34,21 +51,14 @@ class API(object):
                             consumer_secret,
                             access_key,
                             access_secret)
-
+    @exception_handler
     def get_image(self, url):
-        """
-        Retrieve data from the url
-        """
-        try:
-            request = self.request.get(url)
-            return request.content
-        except ConnectionError:
-            print "    connection error, didn't get %s" % (url,)
+        """ Retrieve data from the url """
+        request = self.request.get(url)
+        return request.content
 
     def get_statuses(self, friend_id, max_id=None, since_id=None):
-        """
-        Get all the status a friend published
-        """
+        """ Get all the status a friend published """
         statuses = self.twitter.get_user_timeline(
                         user_id=friend_id,
                         count=200,
@@ -62,25 +72,23 @@ class API(object):
 
         return statuses
 
+    @exception_handler
     def get_statuses_for_friend(self, friend_id, since_id):
-        """
-        Get all statuses since `since_id` for this friend
-        """
-        try:
-            return self.get_statuses(
-                            friend_id,
-                            since_id=since_id)
-        except ConnectionError, err:
-            print "    connection error, didn't get statuses for %s" % friend_id
-        except TwythonRateLimitError, err:
-            print err
-            sys.exit(-1)
-        except TwythonError, err:
-            print "Couln't find statuses for %s" % friend_id
+        """ Get all statuses since `since_id` for this friend """
+        return self.get_statuses(friend_id, since_id=since_id)
 
+    @exception_handler
     def get_friends(self):
         """ Get all friends IDS """
-        try:
-            return self.twitter.get_friends_ids()['ids']
-        except ConnectionError:
-            print "    connection error, didn't get friends_ids"
+        return self.twitter.get_friends_ids()['ids']
+
+    @exception_handler
+    def get_lists(self):
+        """ Get all user lists """
+        return [group['name'] for group in self.twitter.show_lists()]
+
+    @exception_handler
+    def get_list_users(self, list_id):
+        """ Get all users id from a list """
+        return [user['id'] for user in
+                api.twitter.get_list_members(list_id=list_id)['users']]
