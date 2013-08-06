aa0038ce2bd968b0a213e098a81bd8ac3e6e4204
zaurky <zaurky@zeb.re>
2013-08-06 18:27:15 +0200

    add docstrings

--- download_twitter/config.py
+++ download_twitter/config.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+"""
+The download_twitter config module
+"""
 
 import ConfigParser, os
 
 
 def get_config():
+    """ Get the configuration """
     config = ConfigParser.ConfigParser()
     config.read(['/etc/twitter_image.conf',
                  'twitter_image.conf',
