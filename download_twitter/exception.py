f40ccf4a892e35b4990ff0cea0d8672162ac10bc
zaurky <zaurky@zeb.re>
2013-08-27 18:41:32 +0200

    better display of errors

--- download_twitter/exception.py
+++ download_twitter/exception.py
@@ -3,8 +3,20 @@
 import sys
 from functools import wraps
 
-from twython.exceptions import TwythonRateLimitError, TwythonError
+from twython.exceptions import (TwythonRateLimitError,
+                                TwythonError,
+                                TwythonStreamError,
+                                TwythonAuthError)
 from requests.exceptions import ConnectionError
+
+
+def display_method(method, args, kwargs):
+    ret = "%s(%s, %s)" % (
+            method.func_name,
+            ', '.join([unicode(i) for i in args[1:]]),
+            ', '.join(["%s=%s" % (k, v) for k, v in kwargs.items()]),
+    )
+    return ret
 
 
 def exception_handler(method):
@@ -15,11 +27,15 @@ def exception_handler(method):
         try:
             return method(*args, **kwargs)
         except ConnectionError, err:
-            print "    connection error, %s (%s, %s)" % (method, args, kwargs)
+            print "    connection error, %s" % display_method(method, args, kwargs)
         except TwythonRateLimitError, err:
             print err
             sys.exit(-1)
+        except TwythonAuthError, err:
+            print "    auth error, %s" % display_method(method, args, kwargs)
+        except TwythonStreamError, err:
+            print "    stream error, %s" % display_method(method, args, kwargs)
         except TwythonError, err:
-            print "    error, %s (%s, %s)" % (method, args, kwargs)
+            print "    error, %s" % display_method(method, args, kwargs)
 
     return _handle_exceptions
