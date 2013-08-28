34800411ef26496bd42e7e9764387fbe35094a24
zaurky <zaurky@zeb.re>
2013-08-28 14:05:20 +0200

    fdupes is useless here as jpg files containt the tweet in exif data

--- download_images.sh
+++ download_images.sh
@@ -4,5 +4,7 @@ BASEDIR=$(dirname $0)
 
 cd $BASEDIR
 
-source workspace/bin/activate; PYTHONPATH=$BASEDIR python bin/download_image.py && fdupes -r -1 images 2> /dev/null | awk '{print $2}' | xargs rm -f -; deactivate
+source workspace/bin/activate; PYTHONPATH=$BASEDIR python bin/download_image.py; deactivate
+
+# should add a non duplicate script, but jpg files contain tweet as exif, and that change from one to the other
 
