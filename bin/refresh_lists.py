3341a47d0b658cc06f45ba6b6d044a472211a28c
zaurky <zaurky@zeb.re>
2013-09-05 10:53:09 +0200

    be more verbose on : friends that are no longer in the list + friends that are not in a list

--- bin/refresh_lists.py
+++ bin/refresh_lists.py
@@ -8,3 +8,13 @@ if __name__ == '__main__':
     TWITTER = Twitter(get_config())
     TWITTER.refresh_friend()
     TWITTER.refresh_lists()
+    friends = TWITTER.friends.keys()
+    for list_name, content in TWITTER.listcontent['list_content'].items():
+        for friend_id in content:
+            if friend_id not in friends:
+                print "friend %s not in friend list" % friend_id
+            else:
+                friends.remove(friend_id)
+
+    print "friends in not group : "
+    print ", ".join([TWITTER.friends[fid] for fid in friends])
