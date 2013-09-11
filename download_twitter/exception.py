15f32cca92c5a269c6cf73c093f79be066bf16bf
zaurky <zaurky@zeb.re>
2013-09-11 14:08:21 +0200

    change the way we handle rate limit, now just raise an exception

--- download_twitter/exception.py
+++ download_twitter/exception.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
+"""
+Everything that have to do with exceptions.
+"""
 
-import sys
 from functools import wraps
 from datetime import datetime
 
@@ -12,6 +14,7 @@ from requests.exceptions import ConnectionError
 
 
 def display_method(method, args, kwargs):
+    """ return a string showing the twitter api call """
     ret = "%s(%s, %s)" % (
             method.func_name,
             ', '.join([unicode(i) for i in args[1:]]),
@@ -20,21 +23,33 @@ def display_method(method, args, kwargs):
     return ret
 
 
+class RateLimit(Exception):
+    """ rate limit comes from twitter api """
+    _from_twitter = True
+
+
+class InternalRateLimit(RateLimit):
+    """ rate limit comes from our rate limit system """
+    _from_twitter = False
+
+
 def exception_handler(method):
     """ Handle exceptions that could happen here """
 
     @wraps(method)
     def _handle_exceptions(*args, **kwargs):
+        """ decorator """
         try:
             return method(*args, **kwargs)
         except ConnectionError, err:
             print "    connection error, %s" % display_method(method, args, kwargs)
         except TwythonRateLimitError, err:
-            if err.error_code != 1337:
-                print err
-                print "at %s" % datetime.now()
-                sys.exit(-1)
-            sys.exit(0)
+            if err.error_code == 1337:
+                raise InternalRateLimit()
+
+            print err
+            print "at %s" % datetime.now()
+            raise RateLimit()
         except TwythonAuthError, err:
             print "    auth error, %s" % display_method(method, args, kwargs)
         except TwythonStreamError, err:
