9d45db8d9c7d3a1b60d542ab9cf924aed39ee81f
zaurky <zaurky@zeb.re>
2016-09-10 15:36:34 +0200

    Add a lock to avoid starting it twice.

--- download_images.sh
+++ download_images.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $0)
+LOCK="/tmp/download_image.lock"
 
 cd $BASEDIR
 
-source workspace/bin/activate; PYTHONPATH=$BASEDIR python bin/download_image.py; deactivate
+if [ ! -f $LOCK ]; then
+    touch $LOCK
+    source workspace/bin/activate;
+    PYTHONPATH=$BASEDIR python bin/download_image.py > /tmp/download_image.log 2> /tmp/download_image.err;
+    deactivate;
+    rm -f $LOCK
+fi
 
 # should add a non duplicate script, but jpg files contain tweet as exif, and that change from one to the other
 
